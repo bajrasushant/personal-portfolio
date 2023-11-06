736e1d68ee4bde41df69264b563aae1c4974f640
Mansij Maharjan <mansijmaharzn@gmail.com>
2023-11-06 18:25:24 +0545

    add basic signup and login

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require('dotenv').config();
+const UserModel = require('./models/User')
 
 const app = express();
 app.use(express.json());
@@ -8,6 +9,29 @@ app.use(express.json());
 const mongodb_uri = process.env.MONGODB_URI;
 const PORT = 3000
 
+
+app.post('/register', (req, res) => {
+    UserModel.create(req.body)
+        .then(users => res.json(users))
+        .catch(err => res.json(err))
+})
+
+
+app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    UserModel.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                if (user.password === password) {
+                    res.json("Success");
+                } else {
+                    res.json("Incorrect Password");
+                }
+            } else {
+                res.json("No record existed");
+            }
+        })
+})
 
 mongoose.connect(mongodb_uri).then(() => {
     console.log("Database Connected Successfully!");
