80cb1b636d27d669af2df7e75dfd132c9d59fd6e
Sushant Bajracharya <bajra_sushant@gmail.com>
2023-11-06 18:58:02 +0545

    login done

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import Login from './components/pages/Login'
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/pages/Login.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react"
+
+const Login = () => {
+  const [data, setData] = useState({
+    username:"",
+    password:"",
+  });
+
+  const handleSubmit = () => {
+    console.log('Submit');
+  }
+
+  const handleChange= (e) => {
+    e.preventDefault();
+    console.log(e.target.value)
+  }
+  return (
+    <div className="flex items-center justify-center bg-gray-100 h-screen">
+      <section className="bg-white p-8 xs:shadow-md shadow-none rounded">
+          <h1 className="text-2xl mb-2 font-bold">Welcome</h1>
+
+          <p className="text-gray-600 mb-6">
+            Login to your account to continue.
+          </p>
+
+          <form onSubmit={handleSubmit} className="mb-4">
+            <div className="mb-4">
+              <div className="">
+                <label htmlFor="username" className="block text-gray-700 mb-2">
+                  UserName:
+                </label>
+                <input onChange={handleChange} type='text' id="username" value={data.username} className="w-full border border-gray-300 rounded py-2 px-3 mb-3" />
+              </div>
+
+              <div className="">
+                  <label htmlFor="password" className="block text-gray-700 mb-2">
+                    Password
+                  </label>
+                  <input onChange={handleChange} type='password' id="password" value={data.password} className="w-full border border-gray-300 rounded py-2 px-3" />
+
+              </div>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <button type="submit" className="w-[35%] bg-green-500 text-white px-4 py-2 rounded hover:bg-green-700">
+                Login</button>
+            <p className="text-gray-600 text-sm mt-3">
+            Don't have an account? <a href="#" className="text-green-500 hover:text-green-700 font-bold">Sign up</a>
+            </p>
+            </div>
+
+          </form>
+      </section>
+    </div>
+  )
+}
+
+export default Login
