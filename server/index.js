4459db400c6da1b5faed093794f16925cbfa4bb6
Mansij Maharjan <mansijmaharzn@gmail.com>
2023-11-09 18:47:44 +0545

    add JWT authorization

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const jwt = require('jsonwebtoken');
+
 require('dotenv').config();
+
 const UserModel = require('./models/User')
 
 const app = express();
 app.use(express.json());
 
 const mongodb_uri = process.env.MONGODB_URI;
+const secretKey = process.env.JWT_SECRET;
+
 const PORT = 3000
 
 
@@ -26,7 +31,9 @@ app.post('/login', async (req, res) => {
         const user = await UserModel.findOne({ email: email });
         if (user) {
             if (user.password === password) {
-                res.status(200).json({ message: 'Success' });
+                // Create and send a JWT token
+                const token = jwt.sign({ userId: user._id }, secretKey, { expiresIn: '1h' });
+                res.status(200).json({ message: 'Success', token });
             } else {
                 res.status(401).json({ error: 'Incorrect Password' });
             }
@@ -38,6 +45,30 @@ app.post('/login', async (req, res) => {
     }
 });
 
+
+// JWT token
+function verifyToken(req, res, next) {
+    const token = req.header('Authorization');
+    if (!token) {
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    jwt.verify(token, secretKey, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ error: 'Invalid token' });
+        }
+        req.user = decoded;
+        next();
+    });
+}
+
+// Example of protecting a route
+app.get('/protected-route', verifyToken, (req, res) => {
+    res.json({ message: 'This route is protected.' });
+});
+
+
+// server starting and database connections
 mongoose.connect(mongodb_uri).then(() => {
     console.log("Database Connected Successfully!");
     app.listen(PORT, () => {
